2d4a9edfcce2c5837e480cbed8518108685fd51a
Anthony Alarcon Perez <aalarcon@soaint.com>
2024-01-06 10:48:28 -0500

    conexion mongo db

--- src/main/java/com/sport/chocolateros/app/MsChocolaterosAppApplication.java
+++ src/main/java/com/sport/chocolateros/app/MsChocolaterosAppApplication.java
@@ -1,5 +1,14 @@
 package com.sport.chocolateros.app;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import com.mongodb.reactivestreams.client.MongoDatabase;
+import org.bson.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +17,25 @@ public class MsChocolaterosAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MsChocolaterosAppApplication.class, args);
+//        String connectionString = "mongodb+srv://anthonyalarcon8:rPFnqsMtqJc8XNsf@cluster0.6dicmnn.mongodb.net/?retryWrites=true&w=majority";
+//        ServerApi serverApi = ServerApi.builder()
+//                .version(ServerApiVersion.V1)
+//                .build();
+//        MongoClientSettings settings = MongoClientSettings.builder()
+//                .applyConnectionString(new ConnectionString(connectionString))
+//                .serverApi(serverApi)
+//                .build();
+//        // Create a new client and connect to the server
+//        try (MongoClient mongoClient = MongoClients.create(settings)) {
+//            try {
+//                // Send a ping to confirm a successful connection
+//                MongoDatabase database = mongoClient.getDatabase("chocolaterosDb");
+//                database.runCommand(new Document("ping", 1));
+//                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+//            } catch (MongoException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 
 }
